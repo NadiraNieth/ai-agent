0c3e2922e17120ac7d702b760a02c1bccf01a125
NadiraNieth <gmaddaluno@gmx.at>
2026-02-17 18:05:29 +0100

    added roles

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
-
+from google.genai import types
 
 
 def main():
@@ -19,10 +19,10 @@ def main():
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
     args = parser.parse_args()
-
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
     response = client.models.generate_content(
-    model='gemini-2.5-flash', contents= args.user_prompt)
+    model='gemini-2.5-flash', contents=messages)
     
     if response.usage_metadata == None:
         raise RuntimeError("API request failed")
