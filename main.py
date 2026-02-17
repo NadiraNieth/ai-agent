07878accd6226cd6af62f4a34064dde5bc478108
NadiraNieth <gmaddaluno@gmx.at>
2026-02-17 18:30:25 +0100

    verbose args added

--- main.py
+++ main.py
@@ -17,18 +17,26 @@ def main():
     client = genai.Client(api_key=api_key)
 
     parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     parser.add_argument("user_prompt", type=str, help="User prompt")
     args = parser.parse_args()
+    
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
     response = client.models.generate_content(
     model='gemini-2.5-flash', contents=messages)
-    
+    prompt_tokens = response.usage_metadata.prompt_token_count
+    response_tokens = response.usage_metadata.candidates_token_count
+    user_prompt = args.user_prompt
+
     if response.usage_metadata == None:
         raise RuntimeError("API request failed")
 
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if args.verbose == True:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {response_tokens}")
+    
     print("Response:")
     print(response.text)
     
