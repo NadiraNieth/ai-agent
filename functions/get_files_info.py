afdeda29dbe56378b7af3f76f8c8a22cd3a49120
NadiraNieth <gmaddaluno@gmx.at>
2026-02-24 20:24:31 +0100

    pathlogic

---
+++ functions/get_files_info.py
@@ -0,0 +1,27 @@
+import os
+
+def get_files_info(working_directory, directory="."):
+    try:
+        absolute_path = os.path.abspath(working_directory)
+        target_direct = os.path.normpath(os.path.join(absolute_path, directory))
+
+        valid_path = os.path.commonpath([absolute_path, target_direct]) == absolute_path
+
+        if valid_path == False:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        
+        if not os.path.isdir(target_direct):
+            return f'Error: "{directory}" is not a directory'
+
+        items = os.listdir(target_direct)
+        lines = []
+
+        for item_name in items:
+            item_path = os.path.join(target_direct, item_name)
+            
+            data_line = f"- {item_name}: file_size={os.path.getsize(item_path)} bytes, is_dir={os.path.isdir(item_path)}"
+            lines.append(data_line)
+
+        return "\n".join(lines)
+    except Exception as e:
+        return f"Error: {e}"
