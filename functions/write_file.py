f25365b25c02068e9e549bad99cf26e676598d0f
NadiraNieth <gmaddaluno@gmx.at>
2026-03-06 17:12:22 +0100

    write file function

---
+++ functions/write_file.py
@@ -0,0 +1,23 @@
+import os
+
+def write_file(working_directory, file_path, content):
+    try:
+        absolute_path = os.path.abspath(working_directory)
+        target_direct = os.path.normpath(os.path.join(absolute_path, file_path))
+        valid_path = os.path.commonpath([absolute_path, target_direct]) == absolute_path
+
+        if valid_path == False:
+            return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+
+        if os.path.isdir(target_direct):
+            return f'Error: Cannot write to "{file_path}" as it is a directory'
+        else:
+            os.makedirs(os.path.dirname(target_direct), exist_ok=True)
+            
+        with open(target_direct, "w") as f:
+            f.write(content)
+                
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+
+    except Exception as e:
+        return f"Error: {e}"
