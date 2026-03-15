9bf086a1a3dac1db04b95819f80768451cc96dc8
NadiraNieth <gmaddaluno@gmx.at>
2026-03-15 12:16:52 +0100

    function call

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from prompts import system_prompt
-
+from functions.call_function import available_functions
 
 def main():
     print("Hello from ai-agent!")
@@ -26,7 +26,7 @@ def main():
 
     response = client.models.generate_content(
     model='gemini-2.5-flash', contents=messages, 
-    config=types.GenerateContentConfig(system_instruction=system_prompt, temperature=0),)
+    config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt, temperature=0),)
 
     prompt_tokens = response.usage_metadata.prompt_token_count
     response_tokens = response.usage_metadata.candidates_token_count
@@ -41,7 +41,11 @@ def main():
         print(f"Response tokens: {response_tokens}")
     
     print("Response:")
-    print(response.text)
+    if response.function_calls:
+        for function_call in response.function_calls:
+            print (f"Calling function: {function_call.name}({function_call.args})")
+    else:
+        print(response.text)
     
     
 
